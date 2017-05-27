9686ec21d3c19c75d301bf76def90cba4a2d42a0
Alexander Shchegol <zelenzoom@gmail.com>
2017-05-28 00:40:32 +0300

    webpack 2.1 - 2.5

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./home",
     output: {
-        filename: "build.js"
-    }
+        filename: "build.js",
+        library: "home"
+    },
+
+    watch: NODE_ENV == 'development',
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == "development" ? "cheap-module-inline-source-map" : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV:   JSON.stringify(NODE_ENV),
+            LANG:       JSON.stringify('ru')
+        })
+    ]
 };
